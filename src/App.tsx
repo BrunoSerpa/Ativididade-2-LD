5ab7432662b1803a6f9f86e949245b88cd86b05c
BrunoSerpa <serpabruno2005@gmail.com>
2024-08-23 17:37:12 -0300

    J - Inseri o ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-export default function App() {
+import { ThemeProvider } from 'styled-components';
+import { Provider } from './contexts/Contexto';
+import Principal from './pages/Principal';
+import { mega } from './styles/theme';
+
+function App() {
+  const theme = mega
   return (
-    <></>
+    <ThemeProvider theme={theme}>
+      <Provider>
+        <Principal />
+      </Provider>
+    </ThemeProvider>
   );
-};
+}
+
+export default App;
+
